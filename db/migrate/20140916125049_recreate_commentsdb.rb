0a121d651bd0f8599afcadfc1839f36b3e4f9490
prismaticence <cuddlelooker@gmail.com>
2014-09-22 07:12:44 +0800

    recommit

--- db/migrate/20140916125049_recreate_commentsdb.rb
+++
@@ -1,9 +0,0 @@
-class RecreateCommentsdb < ActiveRecord::Migration
-  def change
-  	create_table :comments do |t|
-  		t.string :body
-  		t.integer :user_id, :post_id
-  		t.timestamps
-  	end
-  end
-end
